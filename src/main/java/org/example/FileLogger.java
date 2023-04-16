78a8ab9f5fee9c6c84c6043164243f760fa1917d
DaVinChi <fox.winner9@gmail.com>
2023-04-16 16:37:59 +0300

    Fixed path to settings and file.log

--- src/main/java/org/example/FileLogger.java
+++ src/main/java/org/example/FileLogger.java
@@ -3,16 +3,19 @@ package org.example;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileLogger {
     private static FileLogger INSTANCE;
     private File logFile;
     private FileWriter fileWriter;
+    private static final Path FILE_LOG_PATH = Paths.get("./src/main/resources/file.log");
 
     public static FileLogger getInstance(){
         if(INSTANCE == null) {
             try {
-                INSTANCE = new FileLogger("D:\\Projects\\ChatikWIFI\\src\\main\\resources\\file.log");
+                INSTANCE = new FileLogger();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -21,8 +24,8 @@ public class FileLogger {
         return INSTANCE;
     }
 
-    private FileLogger(String path) throws IOException {
-        logFile = new File(path);
+    private FileLogger() throws IOException {
+        logFile = new File(FILE_LOG_PATH.toUri());
         fileWriter = new FileWriter(logFile, true);
     }
 
--- src/main/java/org/example/ServerSettings.java
+++ src/main/java/org/example/ServerSettings.java
@@ -1,14 +1,17 @@
 package org.example;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ServerSettings {
     private static int PORT = -1;
     private static String HOST = null;
+    private static final Path SETTINGS_PATH = Paths.get(".\\src\\main\\resources\\settings.txt");
 
     public static int getPort() {
         if (PORT == -1) {
-            try (BufferedReader fr = new BufferedReader(new FileReader("D:\\Projects\\ChatikWIFI\\src\\main\\resources\\settings.txt"))) {
+            try (BufferedReader fr = new BufferedReader(new FileReader(SETTINGS_PATH.toFile()))) {
                 PORT = fr.lines()
                         .filter(line -> line.contains("SERVER_PORT"))
                         .map(line -> line.substring(line.indexOf("=") + 2))
@@ -24,7 +27,7 @@ public class ServerSettings {
 
     public static String getHost(){
         if (HOST == null){
-            try (BufferedReader fr = new BufferedReader(new FileReader("D:\\Projects\\ChatikWIFI\\src\\main\\resources\\settings.txt"))) {
+            try (BufferedReader fr = new BufferedReader(new FileReader(SETTINGS_PATH.toFile()))) {
                 HOST = fr.lines()
                         .filter(line -> line.contains("SERVER_HOST"))
                         .map(line -> line.substring(line.indexOf("=") + 2))
